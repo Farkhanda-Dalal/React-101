9028f37dee758d92cf9372f84f2f6b092b58bb21
Farkhanda-Dalal <125860512+Farkhanda-Dalal@users.noreply.github.com>
2024-09-25 23:01:06 +0530

    Add files via upload

---
+++ React_Tailwind/src/App.jsx
@@ -0,0 +1,13 @@
+let greeting=()=>{
+  return <div>
+    <h1 className="text-red-300 bg-slate-300">HELLO</h1>
+    <p>This was styled using Tailwind</p>
+    <br />
+    <button type="button" class="btn btn-primary">
+      Notifications <span class="badge text-bg-secondary">4</span>
+    </button>
+    <p>This was added using Bootstrap</p>
+  </div>
+}
+
+export default greeting;

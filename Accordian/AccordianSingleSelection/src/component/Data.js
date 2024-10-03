ef839c3563f5950bcbb034ee15d69e3ad8ed96c2
Farkhanda-Dalal <125860512+Farkhanda-Dalal@users.noreply.github.com>
2024-10-03 17:28:48 +0530

    Added Data.js

---
+++ Accordian/AccordianSingleSelection/src/component/Data.js
@@ -0,0 +1,20 @@
+const data = [
+  {
+    id: 1,
+    title: "1st accordian",
+    body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, inventore natus. Consequatur modi rerum nisi nesciunt officiis iure, labore quidem quisquam quia nihil? Officia amet nisi debitis, quaerat tenetur eos."
+  },
+  {
+    id: 2,
+    title: "2nd accordian",
+    body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, inventore natus. Consequatur modi rerum nisi nesciunt officiis iure, labore quidem quisquam quia nihil? Officia amet nisi debitis, quaerat tenetur eos."
+  },
+  {
+    id: 3,
+    title: "3rd accordian",
+    body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, inventore natus. Consequatur modi rerum nisi nesciunt officiis iure, labore quidem quisquam quia nihil? Officia amet nisi debitis, quaerat tenetur eos."
+  }
+]
+
+
+export default data;

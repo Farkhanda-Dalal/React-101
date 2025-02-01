9d8dc6402341241a779687b96cadc2bbb199ed5d
Farkhanda-Dalal <125860512+Farkhanda-Dalal@users.noreply.github.com>
2025-02-01 21:07:08 +0530

    Update ListItem.jsx

--- PassingPropsFromParentToChild/src/components/ListItem.jsx
+++ PassingPropsFromParentToChild/src/components/ListItem.jsx
@@ -2,18 +2,24 @@ import React,{Fragment} from 'react'
 
 // Props passed using props obj and then desruvting props obj in a var
 
-const Item = (props) => {
-    let {listItem}=props
+const Item = ({listItem,handle}) => {
+    
 
   return (
-    <Fragment>
-        {
-            listItem==="egg"? (<li key={listItem} className="list-group-item">Egg is smelly</li>): 
-            listItem==="apple" ? (<li key={listItem} className="list-group-item">Apple a day keeps the doc away</li>) :
-            (<li key={listItem} className="list-group-item">{listItem} </li>)
-        }
-        
-    </Fragment>
+    <div className='row p-4 '  >
+      <div className='col'>
+      {
+        listItem==="egg"? (<li key={listItem} className="list-group-item">Egg is smelly</li>): 
+        listItem==="apple" ? (<li key={listItem} className="list-group-item">Apple a day keeps the doc away</li>) :
+        (<li key={listItem} className="list-group-item">{listItem} </li>)
+      }
+      </div>
+      <div className='col'>
+        <button className='bg bg-success'
+        onClick={handle}
+        >Buy</button>
+      </div>
+    </div>
   )
 }
 

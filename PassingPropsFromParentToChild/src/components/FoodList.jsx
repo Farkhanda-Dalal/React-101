bac6d1c99d5d604b5a680522be5da3dfda1c003e
Farkhanda-Dalal <125860512+Farkhanda-Dalal@users.noreply.github.com>
2025-02-01 21:06:10 +0530

    Update FoodList.jsx

--- PassingPropsFromParentToChild/src/components/FoodList.jsx
+++ PassingPropsFromParentToChild/src/components/FoodList.jsx
@@ -4,10 +4,14 @@ import Item from './ListItem'
 // Props passed using props obj and property of item was chained using . operator
 const FoodList = (props) => {
 
+  function handle(item){
+    return console.log(`${item} was bought`)
+  }
+
   return (
     <ul className="list-group">
              {props.List.map((item)=>(
-              <Item listItem={item}></Item>
+              <Item listItem={item} handle={()=>handle(item)}></Item>
             ))}
     </ul>
   )
